5333e95b111dccd5d9cfdefac323542ceea84bfa
unknown <onlyomkhotcs@gmail.com>
2025-01-12 16:52:09 +0530

    Updated code

--- src/Pages/Auths/VerifyLoginTokenPage.jsx
+++ src/Pages/Auths/VerifyLoginTokenPage.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const checkAuthStatus = async () => {
     try {
-        const response = await axios.get('http://localhost:3000/auth/verifyToken',{
+        const response = await axios.get('https://social-media-backend-3fj6.onrender.com/auth/verifyToken',{
             withCredentials: true
         });
         console.log("checkAuthStatus",response.data);
